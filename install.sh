331c3e38b2f64d92c0e47fb10f152600c7efb0a4
Parvez Ali <urparvezali@gmail.com>
2025-12-19 11:58:41 +0600

    myfirstconfig

---
+++ install.sh
@@ -0,0 +1,3 @@
+yay -Sy hyprland waybar swaync swayosd hyprlock hypridle xdg-desktop-portal-hyprland gnome-keyring hyprpaper wl-clipboard grim slurp swappy udiskie blueman network-manager-applet hyprpolkitagent alacritty nwg-look nwg-hello nwg-displays hyprpicker cliphist nautilus tuned-ppd
+rm -rf .config/{alacritty, ashell, cliphist, hypr, rofi, swappy, swayosd, waybar}
+cp -rf . .config
